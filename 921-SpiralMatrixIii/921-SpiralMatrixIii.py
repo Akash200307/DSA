fbcf10bf81202f16dba875edf342fda288fc7005
Akash S <akash200307@outlook.com>
2026-02-03 21:38:10 +0530

    Create 921-SpiralMatrixIii

---
+++ 921-SpiralMatrixIii/921-SpiralMatrixIii.py
@@ -0,0 +1,28 @@
+# Last updated: 2/3/2026, 9:38:09 PM
+class Solution(object):
+    def spiralMatrixIII(self, rows, cols, rStart, cStart):
+        i,j = rStart, cStart
+
+        diri, dirj = 0, 1 # directions to move
+        twice = 2
+        res = []
+        moves = 1
+        next_moves = 2
+
+        while len(res) < (rows * cols):
+            if (-1 < i < rows) and ( -1 < j < cols):
+                res.append([i,j])
+            
+            i += diri
+            j += dirj
+            moves -= 1
+            if moves == 0:
+                diri, dirj = dirj, -diri # 90 deg Clockwise
+                twice -= 1
+                if twice == 0:
+                    twice = 2
+                    moves = next_moves
+                    next_moves += 1
+                else:
+                    moves = next_moves - 1
+        return res
