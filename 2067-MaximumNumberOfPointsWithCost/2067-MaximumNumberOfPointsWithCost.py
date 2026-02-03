e349d46c08feebfe7143f0721ec48be818b9248e
Akash S <akash200307@outlook.com>
2026-02-03 21:36:58 +0530

    Create 2067-MaximumNumberOfPointsWithCost

---
+++ 2067-MaximumNumberOfPointsWithCost/2067-MaximumNumberOfPointsWithCost.py
@@ -0,0 +1,17 @@
+# Last updated: 2/3/2026, 9:36:58 PM
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        r, c=len(points), len(points[0])
+        for i in range(1, r):
+            right=[0]*c
+            right[-1]=points[i-1][-1]
+            for j in range(c-2, -1, -1):
+                right[j]=max(right[j+1]-1, points[i-1][j])
+            
+            left=points[i-1][0]
+            points[i][0]=max(left, right[0])+points[i][0]
+            for j in range(1, c):
+                left=max(left-1, points[i-1][j])
+                points[i][j]=max(left, right[j])+points[i][j]
+                
+        return max(points[-1])
