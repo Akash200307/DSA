d5b4ee08cc04cc57e00619d118ace41d9641d416
Akash S <akash200307@outlook.com>
2026-02-03 21:38:03 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.py
@@ -0,0 +1,17 @@
+# Last updated: 2/3/2026, 9:38:03 PM
+class Solution:
+    def fib(self, n: int) -> int:
+        self.dp=[-1]*(n+1)
+        return self.helper(n)
+    def helper(self,n):
+        if n==1:
+            return 1
+        if n==0:
+            return 0
+        if self.dp[n]!=-1:
+            return self.dp[n]
+        self.dp[n]= self.helper(n-1)+self.helper(n-2)
+
+        return self.dp[n]
+
+        
