c360a7db488111b0ae320738fec57ed45943012e
Akash S <akash200307@outlook.com>
2026-02-08 22:08:59 +0530

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/version/python/3-LongestSubstringWithoutRepeatingCharacters_20260208_220858.py
@@ -0,0 +1,21 @@
+# Last updated: 2/8/2026, 10:08:58 PM
+1class Solution:
+2    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+3        res=[]
+4        self.findCombo(candidates,res,[],target,0,)
+5        return res
+6    def findCombo(self,arr,res,sol,target,i):
+7        if i==len(arr):
+8            if target==0:
+9                res.append(sol.copy())
+10            return
+11            
+12        if arr[i]<=target:
+13            sol.append(arr[i])
+14            self.findCombo(arr,res,sol,target-arr[i],i)
+15            sol.pop()
+16        self.findCombo(arr,res,sol,target,i+1)
+17        
+18            
+19        
+20        
