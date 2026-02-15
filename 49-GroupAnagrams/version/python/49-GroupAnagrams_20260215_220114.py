4ec04de64b6f7a7e32e44a71a191bd2bfc66d459
Akash S <akash200307@outlook.com>
2026-02-15 22:01:15 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/version/python/49-GroupAnagrams_20260215_220114.py
@@ -0,0 +1,16 @@
+# Last updated: 2/15/2026, 10:01:14 PM
+1class Solution:
+2    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+3        
+4
+5        map=defaultdict(list)
+6
+7        for i in strs:
+8            cur="".join(sorted(i))
+9            map[cur].append(i)
+10        
+11        return list(map.values())
+12        
+13        
+14
+15            
