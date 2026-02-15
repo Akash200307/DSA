0afa93126d334d6647325efd03581c5603c6a921
Akash S <akash200307@outlook.com>
2026-02-15 22:54:34 +0530

    File updated at 2/15/2026, 10:54:34 PM

--- 211-DesignAddandSearchWordsDataStructure/211-DesignAddandSearchWordsDataStructure.py
+++ 211-DesignAddandSearchWordsDataStructure/211-DesignAddandSearchWordsDataStructure.py
@@ -1,4 +1,4 @@
-# Last updated: 2/15/2026, 10:51:32 PM
+# Last updated: 2/15/2026, 10:54:34 PM
 1class Trie:
 2    def __init__ (self):
 3        self.children={}
@@ -21,26 +21,25 @@
 20    def search(self, word: str) -> bool:
 21
 22        def dfs(j,root):
-23            curr= root
+23            cur=root
 24            for i in range(j,len(word)):
 25                c=word[i]
 26
-27                if c=='.':
-28                    for child in curr.children.values():
+27                if c==".":
+28                    for child in cur.children.values():
 29                        if dfs(i+1,child):
 30                            return True
 31                    return False
-32                
+32
 33                else:
-34                    if c not in curr.children:
+34                    if c not in cur.children:
 35                        return False
-36                    curr=curr.children[c]
-37            return curr.end
-38
-39        return dfs(0,self.root)
+36                    cur=cur.children[c]
+37            return cur.end
+38        return dfs(0,self.root)
+39       
 40
-41
-42# Your WordDictionary object will be instantiated and called as such:
-43# obj = WordDictionary()
-44# obj.addWord(word)
-45# param_2 = obj.search(word)
+41# Your WordDictionary object will be instantiated and called as such:
+42# obj = WordDictionary()
+43# obj.addWord(word)
+44# param_2 = obj.search(word)
