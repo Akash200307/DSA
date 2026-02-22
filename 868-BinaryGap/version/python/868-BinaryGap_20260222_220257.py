af9b2e25bf782413ffd14fc581b507d9acb23f5e
Akash S <akash200307@outlook.com>
2026-02-22 22:02:58 +0530

    Create 868-BinaryGap

---
+++ 868-BinaryGap/version/python/868-BinaryGap_20260222_220257.py
@@ -0,0 +1,23 @@
+# Last updated: 2/22/2026, 10:02:57 PM
+1class Solution:
+2    def binaryGap(self, n: int) -> int:
+3        max_dist = 0
+4        curr_dist = 0
+5        found_first_one = False
+6        
+7        while n > 0:
+8            bit = n % 2
+9            
+10            if bit == 1:
+11                if found_first_one:
+12                    max_dist = max(max_dist, curr_dist)
+13                
+14                curr_dist = 1
+15                found_first_one = True
+16            else:
+17                if found_first_one:
+18                    curr_dist += 1
+19            
+20            n //= 2
+21            
+22        return max_dist
