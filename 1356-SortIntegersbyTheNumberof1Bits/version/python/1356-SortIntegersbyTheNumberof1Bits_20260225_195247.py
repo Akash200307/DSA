aa5e8b9c4503c6f2e634604f14f3032558c2ce39
Akash S <akash200307@outlook.com>
2026-02-25 19:52:48 +0530

    Create 1356-SortIntegersbyTheNumberof1Bits

---
+++ 1356-SortIntegersbyTheNumberof1Bits/version/python/1356-SortIntegersbyTheNumberof1Bits_20260225_195247.py
@@ -0,0 +1,5 @@
+# Last updated: 2/25/2026, 7:52:47 PM
+1class Solution:
+2    def sortByBits(self, arr: List[int]) -> List[int]:
+3        
+4        return sorted(arr,key=lambda x:(x.bit_count(),x))
