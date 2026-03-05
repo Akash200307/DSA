0241d8d4bdc1d6de9f1b46b293986058d1e85797
Akash S <akash200307@outlook.com>
2026-03-05 22:10:45 +0530

    Create 1758-MinimumChangesToMakeAlternatingBinaryString

---
+++ 1758-MinimumChangesToMakeAlternatingBinaryString/version/python/1758-MinimumChangesToMakeAlternatingBinaryString_20260305_221045.py
@@ -0,0 +1,11 @@
+# Last updated: 3/5/2026, 10:10:45 PM
+1class Solution:
+2    def minOperations(self, s: str) -> int:
+3        a,b,n=0,0,len(s)
+4
+5        for ch in s:
+6            if int(ch)==b:
+7                a+=1
+8            b^=1
+9        
+10        return min(n-a,a)
