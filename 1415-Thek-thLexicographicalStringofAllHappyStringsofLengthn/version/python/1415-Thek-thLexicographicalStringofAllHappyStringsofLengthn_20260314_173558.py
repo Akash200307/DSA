2797346ac6fe31c4c5079eb792e2af91c2fece9e
Akash S <akash200307@outlook.com>
2026-03-14 17:35:59 +0530

    Create 1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn

---
+++ 1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn/version/python/1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn_20260314_173558.py
@@ -0,0 +1,23 @@
+# Last updated: 3/14/2026, 5:35:58 PM
+1class Solution:
+2    def getHappyString(self, n: int, k: int) -> str:
+3        res=[]
+4        sol=[]
+5
+6        def dfs():
+7            if len(sol)==n:
+8                res.append("".join(sol))
+9                return
+10            if len(res) >=k:
+11                return
+12            
+13            for c in 'abc':
+14                if not sol or sol[-1]!=c:
+15                    sol.append(c)
+16                    dfs()
+17                    sol.pop()
+18        dfs()
+19
+20        return res[k-1] if len(res)>=k else ""
+21            
+22            
