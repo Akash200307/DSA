12264684a70559c4efeff72fe9d669ccca475776
Akash S <akash200307@outlook.com>
2026-03-23 13:35:08 +0530

    Create 11-ContainerWithMostWater

---
+++ 11-ContainerWithMostWater/version/python/11-ContainerWithMostWater_20260323_133508.py
@@ -0,0 +1,16 @@
+# Last updated: 3/23/2026, 1:35:08 PM
+1class Solution:
+2    def maxArea(self, height: List[int]) -> int:
+3        l,r=0,len(height)-1
+4        res=float('-inf')
+5        while l<r:
+6            length=r-l
+7            width=min(height[l],height[r])
+8            area=length*width
+9
+10            res=max(area,res)
+11            if height[l]<height[r]:
+12                l+=1
+13            else:
+14                r-=1
+15        return res
