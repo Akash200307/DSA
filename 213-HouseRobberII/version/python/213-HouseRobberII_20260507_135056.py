b55fe7144a2806e0085b359c6b22ea64dce59d6d
Akash S <akash200307@outlook.com>
2026-05-07 13:50:57 +0530

    Create 213-HouseRobberII

---
+++ 213-HouseRobberII/version/python/213-HouseRobberII_20260507_135056.py
@@ -0,0 +1,17 @@
+# Last updated: 5/7/2026, 1:50:56 PM
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 3:
+            return max(nums)
+        # circle of houses
+        # cannot rob 2 adjacent houses
+
+        def robber(arr):
+            prev2 = prev1= 0
+            for point in arr:
+                curr = max(prev2 + point, prev1)
+                prev2 = prev1
+                prev1 = curr
+            return curr
+
+        return max(robber(nums[:-1]), robber(nums[1:]))
