43eb39c434bab3c6baf9235a99347e004365307b
Akash S <akash200307@outlook.com>
2026-06-03 21:14:04 +0530

    Create 28-FindtheIndexoftheFirstOccurrenceinaString

---
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/version/python/28-FindtheIndexoftheFirstOccurrenceinaString_20260603_211403.py
@@ -0,0 +1,10 @@
+# Last updated: 6/3/2026, 9:14:03 PM
+1class Solution:
+2    def strStr(self, haystack: str, needle: str) -> int:
+3        if len(haystack)<len(needle):
+4            return -1
+5        for i in range(len(haystack)):
+6            if haystack[i:i+len(needle)]==needle:
+7                return i
+8        return -1
+9        
