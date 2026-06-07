ccce1516ed5678f9e9134a4d3faaf02fe643ee93
Akash S <akash200307@outlook.com>
2026-06-07 20:14:32 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/version/python/1-TwoSum_20260607_201432.py
@@ -0,0 +1,12 @@
+# Last updated: 6/7/2026, 8:14:32 PM
+1class Solution:
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        map={}
+4
+5        for k,v in enumerate(nums):
+6            diff=target-v
+7            if diff in map:
+8                return [map[diff],k]
+9            map[v]=k
+10        return []
+11
