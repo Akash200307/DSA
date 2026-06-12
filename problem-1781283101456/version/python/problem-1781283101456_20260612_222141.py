8c334bd040d15083764cbf0ab8829ae0dc3d7c5b
Akash S <akash200307@outlook.com>
2026-06-12 22:21:42 +0530

    Create

---
+++ problem-1781283101456/version/python/problem-1781283101456_20260612_222141.py
@@ -0,0 +1,16 @@
+# Last updated: 6/12/2026, 10:21:41 PM
+1class Solution:
+2    def uniquePaths(self, m: int, n: int) -> int:
+3        memo = [[-1] * n for _ in range(m)]
+4        def dfs(i, j):
+5            if i == (m - 1) and j == (n - 1):
+6                return 1
+7            if i >= m or j >= n:
+8                return 0
+9            if memo[i][j] != -1:
+10                return memo[i][j]
+11
+12            memo[i][j] =  dfs(i, j + 1) + dfs(i + 1, j)
+13            return memo[i][j]
+14
+15        return dfs(0, 0)
